04bd1daf47f9af47637a4658bf3eca33c2bd138e
Malcolm Seyd <malcolm@seyd.ca>
2024-08-02 01:44:47 -0700

    Implement custom IntoReponse error type
    Currently it implements From<anyhow::Error> and From<http::StatusCode>,
    which means we can use ? to propagate errors easily inside handlers.
    
    Note that it specifically hides the error message from the user, which
    is good security practice just-in-case. Future work may be needed on
    intercepting errors from extrators to hide the messages as well.

--- src/routes/term.rs
+++ src/routes/term.rs
@@ -1,25 +1,28 @@
 use crate::{middlewares::CookieUserState, scraper::Term};
-use axum::extract::{Extension, Path, State};
+use axum::{
+    extract::{Extension, Path, State},
+    http::StatusCode,
+};
 use maud::{html, Markup};
 use std::sync::Arc;
 use tracing::{debug, instrument};
 
 use crate::components;
 
-use super::DatabaseAppState;
+use super::{AppError, DatabaseAppState};
 
 #[instrument(level = "debug", skip(state))]
 pub async fn term(
-    Path(id): Path<String>,
+    Path(id): Path<String>, // TODO: implement Deserialize on Term
     State(state): State<Arc<DatabaseAppState>>,
     Extension(user_state): CookieUserState,
-) -> Markup {
+) -> Result<Markup, AppError> {
     debug!("term");
-    let term: Term = id.parse().unwrap();
+    let term = id.parse::<Term>().map_err(|_| StatusCode::BAD_REQUEST)?;
 
-    let courses = state.courses(term);
+    let courses = state.courses(term)?;
 
-    components::base(html! {
+    Ok(components::base(html! {
         (components::container::render(&courses))
-    })
+    }))
 }
