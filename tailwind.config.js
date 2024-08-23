1d34560111e5423c2012fa47f388b7e41b81491f
Hal <haln_01@proton.me>
2024-08-23 14:30:26 -0700

    feat: build steps and daisyui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.rs"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
