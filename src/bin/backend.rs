0c82dae91664311787f4de08b049809c4edc80f2
Brennan McMicking <brennanmcmicking@gmail.com>
2025-02-01 12:08:19 -0800

    feat: enable scraper in production

--- src/bin/backend.rs
+++ src/bin/backend.rs
@@ -1,8 +1,10 @@
-use std::{env, net::SocketAddr, path::PathBuf};
+use std::{env, net::SocketAddr, path::PathBuf, time::Duration};
 
 use anyhow::Context;
 use axum_server::tls_rustls::RustlsConfig;
-use scheduler::routes::{self, Stage};
+use scheduler::{routes::{self, Stage}, scraper::scrape};
+use tokio::{task, time};
+use tracing::debug;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -10,13 +12,14 @@ async fn main() -> anyhow::Result<()> {
     let stage: Stage = env::var("STAGE").map_or(Stage::LOCAL, |v| 
         v.into()
     );
-    let use_local_dynamo = env::var("USE_LOCAL_DYNAMO").map_or(false, |v| 
+    let use_local_dynamo = env::var("USE_LOCAL_DYNAMO").is_ok_and(|v| 
        v == "true"
     );
 
     match stage {
-        Stage::PROD => tracing_subscriber::registry()
-            .with(
+        Stage::PROD => {
+            tracing_subscriber::registry()
+                .with(
                 tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                     [
                         "backend=info",         // code in this file
@@ -24,12 +27,22 @@ async fn main() -> anyhow::Result<()> {
                         "tower_http=info",      // http request/response pairs
                         "axum::rejection=trace", // extractor rejections (i.e. bad form input)
                     ]
-                    .join(",")
-                    .into()
-                }),
-            )
-            .with(tracing_subscriber::fmt::layer())
-            .init(),
+                        .join(",")
+                        .into()
+                    }),
+                )
+                .with(tracing_subscriber::fmt::layer())
+                .init();
+
+            let _forever = task::spawn(async {
+                let mut interval = time::interval(Duration::from_secs(3600)); // once per hour
+                loop {
+                    interval.tick().await;
+                    debug!("running scraper");
+                    let _r = scrape(true, None).await;
+                }
+            });
+        },
         _ => tracing_subscriber::registry()
             .with(
                 tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
@@ -60,7 +73,6 @@ async fn main() -> anyhow::Result<()> {
     let soc: SocketAddr = "0.0.0.0:8443"
     .parse()
     .context("invalid binding socket address")?;
-
 
     // if the TLS certs are present, bind with HTTPS
     // otherwise, run normally
